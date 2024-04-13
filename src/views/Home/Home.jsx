bb32834a26eca1dbf181b6067575f2113398ebc5
FabiRami94 <fabioerv_1994@hotmail.com>
2024-04-12 22:36:24 -0500

    inicio de grid

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -7,14 +7,18 @@ import { IoIosArrowForward } from "react-icons/io";
 const Home = () => {
     return(
         <>
-            <h1>Soy el home</h1>
-            <NavLink to={'/info'}>
-                <button className={style.buttons}> INICIAR 
-                    {/* <div style={{display: 'inline-block', height: '20px'}}>
-                        <IoIosArrowForward style={{paddingTop:'20%'}} size={20}/>
-                    </div> */}
-                </button>
-            </NavLink>
+            <div className={style.container}>
+                <div className={style.name}>
+                    {/* <h1>Soy el home</h1> */}
+                </div>
+                <NavLink to={'/info'}>
+                    <button className={style.buttons}> INICIAR 
+                        {/* <div style={{display: 'inline-block', height: '20px'}}>
+                            <IoIosArrowForward style={{paddingTop:'20%'}} size={20}/>
+                        </div> */}
+                    </button>
+                </NavLink>
+            </div>
         </>
     )
 };
