8f76b714fb9bd46d7c85967676087162b02da1e7
FabiRami94 <fabioerv_1994@hotmail.com>
2024-04-13 16:37:08 -0500

    avance grid

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,20 +4,35 @@ import { NavLink } from "react-router-dom";
 import style from './Home.module.css';
 import { IoIosArrowForward } from "react-icons/io";
 
-const Home = () => {
+const Home = () => {  
+    // https://i.ibb.co/PQ34VY1/Fondo-Vansa.png
     return(
         <>
             <div className={style.container}>
-                <div className={style.name}>
-                    {/* <h1>Soy el home</h1> */}
+                <div className={style.manImage}></div>
+                <div className={style.welcome}>
+                    <h1>Te damos la</h1>
+                    <h1>bienvenida a Vansa:</h1>
                 </div>
-                <NavLink to={'/info'}>
-                    <button className={style.buttons}> INICIAR 
-                        {/* <div style={{display: 'inline-block', height: '20px'}}>
-                            <IoIosArrowForward style={{paddingTop:'20%'}} size={20}/>
-                        </div> */}
-                    </button>
-                </NavLink>
+                <div className={style.womanImage}></div>
+                <div className={style.buttonContainer}>
+                    <NavLink to={'/info'}>
+                        <button className={style.buttons}> INICIAR 
+                            {/* <div style={{display: 'inline-block', height: '20px'}}>
+                                <IoIosArrowForward style={{paddingTop:'20%'}} size={20}/>
+                            </div> */}
+                        </button>
+                    </NavLink>
+                </div>
+                <div className={style.test}>
+                    <h1>Prueba técnica de:</h1>
+                </div>
+                <div className={style.name}>
+                    <div className={style.containerName}>
+                        <h1>FABIÁN RAMÍREZ</h1>
+                    </div>
+                </div>
+                <div className={style.vacant}></div>
             </div>
         </>
     )
