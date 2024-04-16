7508f9da4b72ce7a4b5b2e1f652ef5139db6a9e1
FabiRami94 <fabioerv_1994@hotmail.com>
2024-04-15 21:10:41 -0500

    landing

---
+++ src/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import style from './LandingPage.module.css';
+
+const LandingPage = () => {
+    return(
+        <>
+            <div className={style.container}>
+                
+            </div>      
+        </>
+    )
+};
+
+export default LandingPage;
